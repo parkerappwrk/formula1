086150a98924dc84f0b3e2e2a8d64a4f04812d23
Parker S <parker.s@appwrk.com>
2022-07-11 18:09:25 +0530

    axios and dbjson

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Todo from './todo/todo';
+import Navbar from './navbar/navbar';
 import ViewplayerProfile from './viewplayerProfile';
 
 export default function Home() {
     return (
       <>
+        <Navbar></Navbar>
         <Todo></Todo>
         <ViewplayerProfile></ViewplayerProfile>
       </>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,50 +1,49 @@
 import React from "react";
 import Fialogo from '../../images/fia_logo.png';
+import Navbarcss from "./navbar.module.css";
 
 export default function Navbar() {
     return (
-        <div className="top-header-nav">
-            <div className="topWhiteBar">
-                <div className="container">
-                    <div className="top-inner-White-Bar">
-                        <div className="logo">
+        <div className={Navbarcss.topHeaderNav}>
+            <div className={Navbarcss.topWhiteBar}>
+                <div className={Navbarcss.container}>
+                    <div className={Navbarcss.topInnerWhiteBar}>
+                        <div className={Navbarcss.logo}>
                             <a href="/" target="_blank">
                                 <img src={Fialogo} alt="Federation Internationale de l'Automobile"/>
                             </a>
                         </div>
-                        <div className="wrapper-global-nav">
-                            <div className="global-actions clearfix">
-                                <div className="sign-in logged-out display-upsell-link">
-                                    <a href="#" className="login-link btn btn--alpha btn--icon-left" data-locationinpage="navigation" data-pathtype="internal"><span className="user-icon-text"><i className="icon icon-user"></i>Sign In </span></a>
-                                    <a href="#" className="user-account-actions btn btn--default btn--icon-left btn--transparent" data-toggle="modal" data-target="#userLoggedInOverlay" data-locationinpage="navigation" data-pathtype="internal"><span className="user-icon-text"><i className="icon icon-user"></i><i className="icon icon-notification"></i><span className="loggedin-user"></span></span>
+                        <div className={Navbarcss.wrapperGlobalNav}>
+                            <div className={`${Navbarcss.globalActions} ${Navbarcss.clearfix}`}>
+                                <div className={`${Navbarcss.signIn} ${Navbarcss.loggedOut} ${Navbarcss.displayUpsellLink}`}>
+                                    <a href="#" className={Navbarcss.loginLink} data-locationinpage="navigation" data-pathtype="internal"><span className={Navbarcss.userIconText}><i className={Navbarcss.iconUser}></i>Sign In </span></a>
+                                    <a href="#" className={Navbarcss.userAccountActions} data-toggle="modal" data-target="#userLoggedInOverlay" data-locationinpage="navigation" data-pathtype="internal"><span className={Navbarcss.userIconText}><i className={Navbarcss.iconUser}></i><i className={Navbarcss.iconNotification}></i><span className={Navbarcss.loggedinUser}></span></span>
                                     </a>
-                                    <a href="/" className="pitch-link upsell-link btn btn--default" data-locationinpage="navigation" data-pathtype="formula1.com">Subscribe</a>
-                                    <a href="/" className="subscribe-link upsell-link btn btn--default d-none" data-locationinpage="navigation" data-pathtype="formula1.com">Subscribe</a>
-                                    <a href="/" className="upgrade-link upsell-link btn btn--default d-none" data-locationinpage="navigation" data-pathtype="formula1.com">Upgrade</a>
+                                    <a href="/" className={Navbarcss.pitchLink} data-locationinpage="navigation" data-pathtype="formula1.com">Subscribe</a>
                                 </div>
                             </div>
-                            <div className="global-links">
+                            <div className={Navbarcss.globalLinks}>
                                 <ul>
-                                    <li className="has-image">
-                                        <a href="/" data-locationinpage="navigation" data-pathtype="external" target="“_blank”">
-                                            <img src="/etc/designs/fom-website/images/f1-tv-logo.svg" alt="F1 TV Logo" width="72" height="12"/><span className="icon icon-chevron-right"></span>
+                                    <li className={Navbarcss.hasImage}>
+                                        <a href="/" data-locationinpage="navigation" data-pathtype="external" target="_blank">
+                                            <img src="/etc/designs/fom-website/images/f1-tv-logo.svg" alt="F1 TV Logo" width="72" height="12"/><span className={Navbarcss.iconChevronRight}></span>
                                         </a>
                                     </li>
-                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Authentics<span className="icon icon-chevron-right"></span></a></li>
-                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Store<span className="icon icon-chevron-right"></span></a></li>
-                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Tickets<span className="icon icon-chevron-right"></span></a></li>
-                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Hospitality<span className="icon icon-chevron-right"></span></a></li>
-                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Experiences<span className="icon icon-chevron-right"></span></a></li>
+                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Authentics<span className={Navbarcss.iconChevronRight}></span></a></li>
+                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Store<span className={Navbarcss.iconChevronRight}></span></a></li>
+                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Tickets<span className={Navbarcss.iconChevronRight}></span></a></li>
+                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Hospitality<span className={Navbarcss.iconChevronRight}></span></a></li>
+                                    <li><a href="/" target="_blank" rel="noopener" data-locationinpage="navigation" data-pathtype="external">Experiences<span className={Navbarcss.iconChevronRight}></span></a></li>
                                 </ul>
-                                <a href="/" data-locationinpage="navigation" data-pathtype="external" target="“_blank”">
+                                <a href="/" data-locationinpage="navigation" data-pathtype="external" target="_blank">
                                     <img src="/etc/designs/fom-website/images/f1-tv-logo.svg" alt="F1 TV Logo" width="72" height="12"/>
                                 </a>
                             </div>
                         </div>
-                        <div className="f2-f3-nav">
+                        <div className={Navbarcss.nav}>
                             <ul>
                                 <li>
-                                    <a href="/en.html" className="f2f3-black">
+                                    <a href="/en.html" className={Navbarcss.f2f3black}>
                                         <span>F1<sup>™</sup></span>
                                     </a>
                                 </li>
